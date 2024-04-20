7a3e2804b2c7d9398b540e891639848ce4cf5074
aaarland <adrian.aarland@gmail.com>
2024-04-20 16:44:21 -0500

    chroe: added ssl for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), basicSsl()],
 })
